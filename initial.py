5f68be1f673de5572ebc5c2c9476d86119959229
MadeAdiSuryaPramana <18220105@mahasiswa.itb.ac.id>
2022-09-25 17:04:05 +0700

    init commit

---
+++ initial.py
@@ -0,0 +1,14 @@
+from pickle import TRUE
+from xmlrpc.client import boolean
+
+
+print("Welcome to")
+print("Teknologi Sistem Terintegrasi")
+
+x = input("Mau liat pesan tersembunyi? .... (Y/N)")
+if (x == "Y"):
+    print("Bandung itu dingin...")
+elif (x == "N"):
+    print("Ok bye!")
+else:
+    print("bye!")
